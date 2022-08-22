7ab16133d504e0a789389cc13197bf91f5d50185
Rain Tüür <rain.tuur@gmail.com>
2022-08-22 11:32:43 +0300

    Initial commit

---
+++ src/main/java/ee/bcs/someProject/SomeProjectApplication.java
@@ -0,0 +1,13 @@
+package ee.bcs.someProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SomeProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SomeProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ee/bcs/someProject/SomeProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package ee.bcs.someProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SomeProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
